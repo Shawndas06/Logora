8fa8a1d42a32562aedf5c1d48e0faf49834e611a
Shawndas <bzurabov@gmail.com>
2025-06-12 19:01:35 +0300

    baga13

--- python/app.py
+++ python/app.py
@@ -200,4 +200,4 @@ def get_payments(account_id):
     return {'payments': [dict(payment) for payment in payments]}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
